71dc660425d35b0be84d2e4a361b5e14ad0673d3
Michelle Garcia <michellemarshgarcia@gmail.com>
2018-03-12 13:12:17 -0600

    changes to signUp routes

--- models/client.js
+++ models/client.js
@@ -1,11 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
     var Client = sequelize.define("Client", {
-        client_name: {
+        name: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [1, 40]
             }
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        phone: {
+             type: DataTypes.STRING,
+             validate: {
+                 len: [10],
+                 is: ["^[0-9]+$",'i']
+             }           
         }
     });
 
